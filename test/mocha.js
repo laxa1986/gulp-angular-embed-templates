04c0f0bb652beac889d87296b3345bab730c5e55
Alexey_Chekulaev <Alexey_Chekulaev@epam.com>
2015-05-17 17:19:56 +0300

    - add unit tests - improved regexp so now it matches "templateUrl", 'templateUrl' and templateUrl{spaces}: - removed unnecessary npm dependencies

---
+++ test/mocha.js
@@ -0,0 +1,116 @@
+var embedTemplates = require('../');
+//var gulp = require('gulp');
+//var should = require('should');
+var assert = require('assert');
+//var streamAssert = require('stream-assert');
+require('mocha');
+var fs = require('fs');
+var File = require('gulp-util').File;
+
+//var fixtures = function (glob) { return path.join(__dirname, 'fixtures', glob); };
+
+describe('gulp-angular-embed-templates', function () {
+    it('should embed template content whenever specified templateUrl', function (done) {
+        // Create a 'gulp-angular-embed-templates' plugin stream
+        var sut = embedTemplates();
+
+        // create the fake file
+        var fakeFile = new File({
+            contents: new Buffer(fs.readFileSync('test/assets/hello-world-directive.js'))
+        });
+
+        // write the fake file to it
+        sut.write(fakeFile);
+
+        // wait for the file to come back out
+        sut.once('data', function (file) {
+            // make sure it came out the same way it went in
+            assert(file.isBuffer());
+
+            // check the contents
+            assert.equal(file.contents.toString('utf8'),
+                'angular.module(\'test\').directive(\'helloWorld\', function () {\r\n' +
+                '    return {\r\n' +
+                '        restrict: \'E\',\r\n' +
+                '        template:\'<strong>Hello World!</strong>\'\r\n' +
+                '    };\r\n' +
+                '});'
+            );
+            done();
+        });
+    });
+
+    it('should dial with single quoted template paths', function (done) {
+        var sut = embedTemplates();
+        var fakeFile = new File({contents: new Buffer('templateUrl: \'test/assets/hello-world-template.html\'')});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), 'template:\'<strong>Hello World!</strong>\'');
+            done();
+        });
+    });
+
+    it('should dial with double quoted template paths', function (done) {
+        var sut = embedTemplates();
+        var fakeFile = new File({contents: new Buffer('templateUrl: "test/assets/hello-world-template.html"')});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), 'template:\'<strong>Hello World!</strong>\'');
+            done();
+        });
+    });
+
+    it('should dial with single quoted templateUrl key', function (done) {
+        var sut = embedTemplates();
+        var fakeFile = new File({contents: new Buffer('\'templateUrl\': \'test/assets/hello-world-template.html\'')});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), 'template:\'<strong>Hello World!</strong>\'');
+            done();
+        });
+    });
+
+    it('should dial with double quoted templateUrl key', function (done) {
+        var sut = embedTemplates();
+        var fakeFile = new File({contents: new Buffer('"templateUrl": \'test/assets/hello-world-template.html\'')});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), 'template:\'<strong>Hello World!</strong>\'');
+            done();
+        });
+    });
+
+    it('should dial with templateUrl {SPACES} : {SPACES} {url} ', function (done) {
+        var sut = embedTemplates();
+        var fakeFile = new File({contents: new Buffer('"templateUrl" \t\r\n:\r\n\t  \'test/assets/hello-world-template.html\'')});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), 'template:\'<strong>Hello World!</strong>\'');
+            done();
+        });
+    });
+
+    it('should dial with templateUrl {SPACES} : {SPACES} {url} ', function (done) {
+        var sut = embedTemplates();
+        var fakeFile = new File({contents: new Buffer('"templateUrl" \t\r\n:\r\n\t  \'test/assets/hello-world-template.html\'')});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), 'template:\'<strong>Hello World!</strong>\'');
+            done();
+        });
+    });
+
+    it('should skip errors if particular flag specified', function (done) {
+        var sut = embedTemplates({skipErrors: true});
+        var fakeFile = new File({contents: new Buffer(JSON.stringify({
+            templateUrl: 'test/assets/hello-world-template.html',
+            l2: {templateUrl: 'test/assets/hello-world-template2.html'},
+            l3: {templateUrl: 'test/assets/hello-world-template.html'}
+        }))});
+        sut.write(fakeFile);
+        sut.once('data', function (file) {
+            assert.equal(file.contents.toString('utf8'), '{template:\'<strong>Hello World!</strong>\',"l2":{"templateUrl":"test/assets/hello-world-template2.html"},"l3":{template:\'<strong>Hello World!</strong>\'}}');
+            done();
+        });
+    });
+});
